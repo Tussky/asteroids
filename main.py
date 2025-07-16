7a17ac45fbcd57748b8a70724f3a60b2e7444528
tusky <isaac@dw-perspective.org.uk>
2025-07-16 08:05:39 -0400

    Feature: Added circle class. Bugfix: Added display.flip to render screen.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
 )
+from player import Player
 
 
 def main():
@@ -15,13 +16,19 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", 720)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
-        screen.fill((255, 255, 255))
+        screen.fill((0, 0, 0))
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1_000
+        pygame.display.flip()
 
 
 if __name__ == "__main__":

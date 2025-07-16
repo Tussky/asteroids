93c8bbf36d94a3205bb8711be75bba5cd8428abd
tusky <isaac@dw-perspective.org.uk>
2025-07-16 07:07:03 -0400

    Feature: black screen with gameloop and constant imports

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
 
+from constants import (
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_SPAWN_RATE,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+
 
 def main():
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", 720)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((255, 255, 255))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
